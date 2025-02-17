946aed6d25c963738cfde3b89c6306773c5bddbc
Olha Fatina <fowell@urk.net>
2025-02-17 23:47:34 +0100

    Add createAccountDataProvaiderFromCsv in CreateAccountTest

--- src/main/java/demo/utils/DataProviders.java
+++ src/main/java/demo/utils/DataProviders.java
@@ -1,6 +1,14 @@
 package demo.utils;
 
+import demo.model.Contact;
 import org.testng.annotations.DataProvider;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class DataProviders {
     @DataProvider
@@ -18,5 +26,34 @@ public class DataProviders {
                 {"Computers", "Notebooks", 1}, // Компьютеры → Ноутбуки → 1-й товар
                 {"Electronics", "Cell phones", 3} // Электроника → Мобильные телефоны → 3-й товар
         };
+    }
+
+
+    @DataProvider
+    public Iterator<Object[]> createAccountDataProvaiderFromCsv()throws IOException {
+        // Создаем список для хранения данных для тестов
+        List<Object[]> list = new ArrayList<>();
+        // Открываем CSV файл для чтения
+        BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/contactshw .csv"));
+        // Читаем первую строку из файла
+        String line = reader.readLine();
+        // Обрабатываем каждую строку файла до конца
+        while (line != null) {
+            // Разделяем строку на элементы по запятой
+            String[] split = line.split(",");
+            // Создаем объект Contact и устанавливаем его поля из прочитанных данных
+            list.add(new Object[]{new Contact()
+                    .setFirstName(split[0])
+                    .setLastName(split[1])
+                    .setEmail(split[2])
+                    .setPassword(split[3])
+            });
+            // Читаем следующую строку из файла
+            line = reader.readLine();
+        }
+        // Закрываем файл после чтения всех данных
+        reader.close();
+        // Возвращаем итератор для списка объектов
+        return list.iterator();
     }
 }
--- src/test/java/demo/CreateAccountTests.java
+++ src/test/java/demo/CreateAccountTests.java
@@ -1,6 +1,8 @@
 package demo;
 
 import demo.data.UserData;
+import demo.model.Contact;
+import demo.utils.DataProviders;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
@@ -32,6 +34,31 @@ public class CreateAccountTests extends TestBase {
         app.getUserHelper().click(By.cssSelector(" #register-button"));
        app.driver.findElement(By.linkText(UserData.RANDOM_EMAIL));
         Assert.assertTrue(app.driver.findElement(By.linkText(UserData.RANDOM_EMAIL)).isDisplayed(), "Your registration completed");
+        app.getUserHelper().clickOnLogoutButton();
+    }
+
+    @Test(dataProvider = "createAccountDataProvaiderFromCsv", dataProviderClass = DataProviders.class)
+    public void createAccountDataProvaiderPositiveTest(Contact contact) {
+        app.getUserHelper().click(By.xpath("//a[contains(text(),'Register')]"));
+        app.driver.findElement(By.xpath("//label[contains(text(),\'Gender:\')]"));
+        if (app.driver.findElement(By.xpath("//label[contains(text(),\'Gender:\')]")) == app.driver.findElement(By.xpath("//label[contains(text(),'Male')]"))) {
+            app.getUserHelper().click(By.xpath("//label[contains(text(),'Male')]"));
+        } else {
+            app.getUserHelper().click(By.xpath("//label[contains(text(),'Female')]"));
+        }
+        app.getUserHelper().click(By.name("FirstName"));
+        app.driver.findElement(By.name("FirstName")).sendKeys(contact.getFirstName());
+        app.getUserHelper().click(By.name("LastName"));
+        app.driver.findElement(By.name("LastName")).sendKeys(contact.getLastName());
+        app.getUserHelper().click(By.name("Email"));
+        app.driver.findElement(By.name("Email")).sendKeys(contact.getEmail());
+        app.getUserHelper().click(By.name("Password"));
+        app.driver.findElement(By.name("Password")).sendKeys(contact.getPassword());
+        app.getUserHelper().click(By.name("ConfirmPassword"));
+        app.driver.findElement(By.name("ConfirmPassword")).sendKeys(contact.getPassword());
+        app.getUserHelper().click(By.cssSelector(" #register-button"));
+        app.driver.findElement(By.linkText(contact.getEmail()));
+        Assert.assertTrue(app.driver.findElement(By.linkText(contact.getEmail())).isDisplayed(), "Your registration completed");
         app.getUserHelper().clickOnLogoutButton();
     }
 //"portishead208961"+System.currentTimeMillis()+"@gmail.com"
